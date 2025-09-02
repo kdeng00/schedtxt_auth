76e7dd774928fab03cd75e3640430bb2ae5866d1
phoenix <kundeng00@pm.me>
2025-09-02 22:35:50 +0000

    Added code to broadcast api (#1)
    Reviewed-on: https://git.kundeng.us/phoenix/textsender-auth/pulls/1
    Co-authored-by: phoenix <kundeng00@pm.me>
    Co-committed-by: phoenix <kundeng00@pm.me>

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-
+import "net/http"
+import "log"
 import "fmt"
 
+// Port that the application runs on
+const Port = 9080
 
 func main() {
 	fmt.Println("textsender-auth")
+
+	portListen := fmt.Sprintf(":%d", Port)
+
+	log.Println("Starting server port:", Port)
+	log.Fatal(http.ListenAndServe(portListen, nil))
 }
