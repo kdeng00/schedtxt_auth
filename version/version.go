ed2a44087242a5954c8d6e56982a7de215e30459
phoenix <kundeng00@pm.me>
2025-10-29 19:00:43 -0400

    Added version package

--- version/version.go
+++ version/version.go
@@ -1,7 +1,17 @@
 package version
 
+import "fmt"
+
 var (
-	Version = "dev"
-	Commit = "none"
-	Date = "unknown"
+	Version   = "dev"
+	BuildTime = "unknown"
+	Commit    = "unknown"
+	GoVersion = "unknown"
 )
+
+func String() string {
+	return fmt.Sprintf(
+		"Version: %s\nBuild Date: %s\nCommit: %s\nGo Version: %s",
+		Version, BuildTime, Commit, GoVersion,
+	)
+}
