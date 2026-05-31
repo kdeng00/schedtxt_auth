1a6be5f541d64e80ae0db0abdfc3dddfd605d6e9
phoenix <mail@kundeng.us>
2026-05-30 23:29:21 -0400

    Updating main rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 
 
-pub fn main() {
-    println!("textsender_auth");
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    /*
+    let app = init::app().await;
+
+    // run our app with hyper, listening globally on port 8001
+    let url = config::get_full();
+    let listener = tokio::net::TcpListener::bind(url).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    */
 }
