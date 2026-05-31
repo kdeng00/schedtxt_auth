42dd25691cbd7231b92b01107bb88ce12199d24a
phoenix <mail@kundeng.us>
2026-05-31 16:11:34 -0400

    Adding db module

---
+++ src/db/init.rs
@@ -0,0 +1,24 @@
+
+
+
+use sqlx::postgres::PgPoolOptions;
+
+pub async fn create_pool() -> Result<sqlx::PgPool, sqlx::Error> {
+    let database_url = textsender_models::envy::environment::get_db_url().await.value;
+    println!("Database url: {database_url}");
+
+    PgPoolOptions::new()
+        .max_connections(super::connection_settings::MAXCONN)
+        .connect(&database_url)
+        .await
+}
+
+pub async fn migrations(pool: &sqlx::PgPool) {
+    // Run migrations using the sqlx::migrate! macro
+    // Assumes your migrations are in a ./migrations folder relative to Cargo.toml
+    sqlx::migrate!("./migrations")
+        .run(pool)
+        .await
+        .expect("Failed to run migrations");
+}
+
